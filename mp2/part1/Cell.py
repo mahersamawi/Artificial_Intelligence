04f7f28eb370e970f9957bb7ae2589a7d797ca24
Maher Samawi <msamawi2@illinois.edu>
2017-10-23 17:19:46 -0500

    Working solution for part1.1 5x5

--- mp2/part1/Cell.py
+++ mp2/part1/Cell.py
@@ -1,46 +1,47 @@
+
 class Cell(object):
-	assigned = None
-	pos_x = None
-	pos_y = None
-	color = None
-	source = None
+    assigned = None
+    pos_x = None
+    pos_y = None
+    color = None
+    source = None
 
-	def __init__(self, x, y, color):
-		self.pos_x = x
-		self.pos_y = y
-		if (color == None):
-			self.assigned = False
-			self.source = False
-		else:
-			self.assigned = True
-			self.source = True
-			self.color = color
+    def __init__(self, x, y, color):
+        self.pos_x = x
+        self.pos_y = y
+        if (color == None):
+            self.assigned = False
+            self.source = False
+        else:
+            self.assigned = True
+            self.source = True
+            self.color = color
 
-	def __str__(self):
-		if (self.color == None):
-			return "_"
-		return self.color
+    def __str__(self):
+        if self.color == None:
+            return "_"
+        return self.color
 
-	def get_assigned(self):
-		return self.assigned
+    def get_assigned(self):
+        return self.assigned
 
-	def get_coords(self):
-		return self.pos_x, self.pos_y
+    def get_coords(self):
+        return self.pos_x, self.pos_y
 
-	def is_source(self):
-		return self.source
+    def is_source(self):
+        return self.source
 
-	def get_color(self):
-		return self.color
+    def get_color(self):
+        return self.color
 
-	def set_color(self, color):
-		self.color = color
+    def set_color(self, color):
+        self.color = color
 
-	def set_assigned(self, assigned):
-		self.assigned = assigned
+    def set_assigned(self, assigned):
+        self.assigned = assigned
 
-	def get_position(self):
-		return self.pos_x, self.pos_y
+    def get_position(self):
+        return self.pos_x, self.pos_y
 
-	def print_position(self):
-		print("Cell position: (" + str(self.pos_x) + ", " + str(self.pos_y) + ")" )
+    def print_position(self):
+        print("Cell position: (" + str(self.pos_x) + ", " + str(self.pos_y) + ")" )
