3f2bf767189af021504d9e1a43396a974ec39dee
Taran Saggu <tarandeepsagg@yahoo.com>
2017-10-26 21:08:02 -0500

    Added more helper functions for moving pawns and shit

--- mp2/part2/Board.py
+++ mp2/part2/Board.py
@@ -34,6 +34,19 @@ class Board(object):
                     print("-", end="|")
             print("")
 
+    def get_number_of_pawns(self, color):
+        if color == "w":
+            return len(self.white_pawns)
+        else:
+            return len(self.black_pawns)
+
+    def move_pawn(self, pawn, dest_tuple):
+        pawn_pos = pawn.get_position()
+        self.board_array[pawn_pos[0]][pawn_pos[1]] = None
+        self.board_array[dest_tuple[0]][dest_tuple[1]] = pawn
+        pawn.set_position(dest_tuple[0], dest_tuple[1])
+
+
     # moves are strings: up, upleft, upright, down, downleft, downright
     def get_valid_moves(self, pawn):
         game_board = self
@@ -41,6 +54,11 @@ class Board(object):
         pawn_color = pawn.get_color()
         pawn_x, pawn_y = pawn.get_position()
         if (pawn_color == "w"):
+            # check downleft
+            temp = game_board.get_pawn(pawn_x+1, pawn_y-1)
+            if (temp == None) or ((temp != "invalid") and (temp.get_color() == "b")):
+                valid_moves.append((pawn_x+1, pawn_y-1))
+
             # check down
             temp = game_board.get_pawn(pawn_x+1, pawn_y)
             if (temp == None):
@@ -51,12 +69,14 @@ class Board(object):
             if (temp == None) or ((temp != "invalid") and (temp.get_color() == "b")):
                 valid_moves.append((pawn_x+1, pawn_y+1))
 
-            # check downleft
-            temp = game_board.get_pawn(pawn_x+1, pawn_y-1)
-            if (temp == None) or ((temp != "invalid") and (temp.get_color() == "b")):
-                valid_moves.append((pawn_x+1, pawn_y-1))
+            
 
         if (pawn_color == "b"):
+            # check upleft
+            temp = game_board.get_pawn(pawn_x-1, pawn_y-1)
+            if (temp == None) or ((temp != "invalid") and (temp.get_color() == "w")):
+                valid_moves.append((pawn_x-1, pawn_y-1))
+
             # check up
             temp = game_board.get_pawn(pawn_x-1, pawn_y)
             if (temp == None):
@@ -67,9 +87,6 @@ class Board(object):
             if (temp == None) or ((temp != "invalid") and (temp.get_color() == "w")):
                 valid_moves.append((pawn_x-1, pawn_y+1))
 
-            # check upleft
-            temp = game_board.get_pawn(pawn_x-1, pawn_y-1)
-            if (temp == None) or ((temp != "invalid") and (temp.get_color() == "w")):
-                valid_moves.append((pawn_x-1, pawn_y-1))
+            
 
         return valid_moves
--- mp2/part2/Gamesolver.py
+++ mp2/part2/Gamesolver.py
@@ -1,9 +1,24 @@
 from Pawn import *
 from Board import *
-
+import random
 
 game_board = Board()
 game_board.print_board()
-moves = game_board.get_valid_moves(game_board.get_pawn(6, 4))
+test_pawn = game_board.get_pawn(6, 4)
+moves = game_board.get_valid_moves(test_pawn)
 for move in moves:
     print("Move: " + str(move))
+game_board.move_pawn(test_pawn, moves[2])
+game_board.print_board()
+
+def defensive_heuristic_1(color):
+    num_pawns_rem = game_board.get_number_of_pawns(color)
+    return 2 * num_pawns_rem + random()
+
+def offensive_heuristic_1(color):
+    if color == "w":
+        black_pawns_rem = game_board.get_number_of_pawns("b")
+        return 2 * (30 - black_pawns_rem) + random()
+    else:
+        white_pawns_rem = game_board.get_number_of_pawns("w")
+        return 2 * (30 - white_pawns_rem) + random()
