d05e6fb73debecde256be042cecbad9fe0b3c098
Taran Saggu <tarandeepsagg@yahoo.com>
2017-10-29 18:15:39 -0500

    Tried to run simulator multiple times, not working

--- mp2/part2/Gamesim.py
+++ mp2/part2/Gamesim.py
@@ -3,27 +3,25 @@ from Gamesolver import *
 game_board = Board()
 black_pieces = game_board.get_black_pawns()
 white_pieces = game_board.get_white_pawns()
-# test_pawn = game_board.get_pawn(6, 4)
-# moves = game_board.get_valid_moves(test_pawn)
-# for move in moves:
-#     print("Move: " + str(move))
-# game_board.move_pawn(test_pawn, (1,1))
-# game_board.print_board()
-# print(minimax())
+num_black_wins = 0
+num_white_wins = 0
 
-
-def check_win_conditions():
-    if (len(black_pieces) == 0 or len(black_pieces) == 0):
+def check_win_conditions(b_pieces, w_pieces):
+    if (len(b_pieces) == 0 or len(w_pieces) == 0):
         print("somebody fucked up")
         return False
-    for black_pawn in black_pieces:
+    for black_pawn in b_pieces:
         pos_x, pos_y = black_pawn.get_position()
         if pos_x == 0:
+            global num_black_wins 
+            num_black_wins += 1
             print("black wins")
             return False
-    for white_pawn in white_pieces:
+    for white_pawn in w_pieces:
         pos_x, pos_y = white_pawn.get_position()
         if pos_x == 7:
+            global num_white_wins
+            num_white_wins += 1
             print("white wins")
             return False
     return True
@@ -42,15 +40,24 @@ game_running = True
 # for piece in black_pieces:
 #     pawn_x, pawn_y = piece.get_position()
 #     print("pawn position: (" + str(pawn_x) + ", " + str(pawn_y) + ")")
-while(game_running):
-    print("WHITE TURN")
-    move_white("offensive")
+scenario = 0
+while (scenario < 10):
+    while(game_running):
+        # print("WHITE TURN")
+        move_white("offensive")
+        # game_board.print_board()
+        # print("\n")
+        # print("BLACK TURN")
+        move_black("defensive")
+        # game_board.print_board()
+        # game_board.scan_board()
+        game_running = check_win_conditions(black_pieces, white_pieces)
+        # print("\n")
     game_board.print_board()
-    print("\n")
-    print("BLACK TURN")
-    move_black("defensive")
-    game_board.print_board()
-
-    game_board.scan_board()
-    game_running = check_win_conditions()
-    print("\n")
+    game_board = Board()
+    black_pieces = game_board.get_black_pawns()
+    white_pieces = game_board.get_white_pawns()    
+    scenario += 1
+    game_running = True
+print("Black winrate: " + str((100 * num_black_wins)/scenario))
+print("White winrate: " + str((100 * num_white_wins)/scenario))
