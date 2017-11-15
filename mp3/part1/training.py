ad5c0860e192960f049a4e7f12e9c851803a6b5f
Taran Saggu <tarandeepsaggu@yahoo.com>
2017-11-14 22:27:01 -0600

    part 1 almost done, only confusion matrix and odds ratios left

--- mp3/part1/training.py
+++ mp3/part1/training.py
@@ -1,12 +1,11 @@
-trainingimages = "./digitdata/trainingimages"
-traininglabels = "./digitdata/traininglabels"
+import math
 
 training_labels_array = []
 training_images_array = []
 label_counts = [0 for i in range(10)]
 priors = [0 for i in range(10)]
 num_images = 0
-laplace_constant = 1
+laplace_constant = 0.1
 
 def get_image(current_index):
     current_image = []
@@ -26,17 +25,17 @@ def get_averages():
     for i in range(10):
         priors[i] = label_counts[i]/num_images
 
-
-with open(traininglabels) as text_file:
-    for line in text_file:
-        training_labels_array.append(line.rstrip('\n'))
-
-
-with open(trainingimages) as text_file:
-    for line in text_file:
-        training_images_array.append(line.rstrip('\n'))
+def get_file(file_name):
+    arr = []
+    file_name = "./digitdata/" + file_name
+    with open(file_name) as text_file:
+        for line in text_file:
+            arr.append(line.rstrip('\n'))
+    return arr
 
 
+training_labels_array = get_file("traininglabels")
+training_images_array = get_file("trainingimages")
 num_images = len(training_images_array)/28
 image_index = 0
 totals = [[[0 for k in range(28)] for j in range(28)] for i in range(10)]
@@ -54,10 +53,47 @@ while image_index < num_images:
     image_index += 1
 
 get_averages()
-print("number images: " + str(num_images))
-# for i in range(28):
-#     for j in range(28):
-#         print(totals[5][i][j], end="")
-#     print("")
 
+# Testing
+test_images_array = get_file("testimages")
+test_labels_array = get_file("testlabels")
+test_output = []
+total_test_labels = [0 for i in range(10)]
+test_label_output = [0 for i in range(10)]
+image_index = 0
+num_correct = 0
+num_test_images = len(test_labels_array)
 
+while image_index < num_test_images:
+    class_prob = [0 for i in range(10)]
+    expected_label = int(test_labels_array[image_index])
+    test_image = []
+    for i in range(28):
+        image_line = test_images_array[image_index * 28 + i]
+        test_image.append(image_line)
+    for class_index in range(10):
+        class_prob[class_index] = math.log(priors[class_index])
+        for row in range(28):
+            line = test_image[row]
+            for col in range(28):
+                pixel = line[col]
+                if (pixel != ' '):
+                    class_prob[class_index] += math.log(totals[class_index][row][col])
+                else:
+                    class_prob[class_index] += math.log(1 - totals[class_index][row][col])
+    image_index += 1
+    output = class_prob.index(max(class_prob))
+    test_output.append(output)
+    total_test_labels[expected_label] += 1
+    if (output == expected_label):
+        num_correct += 1
+        test_label_output[expected_label] += 1
+    else:
+        print("Misidentified: |" + str(output) + "|" + str(expected_label) + "|")
+
+for i in range(len(test_label_output)):
+    test_label_output[i] /= total_test_labels[i]
+    print("Label " + str(i) + " Accuracy: " + str(test_label_output[i]))
+
+print("number correct: " + str(num_correct))
+print("percentage correct: " + str(num_correct/num_test_images))
