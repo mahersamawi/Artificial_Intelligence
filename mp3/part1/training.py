7dee343ce2cff7c11fce52f113f11504abe07b1e
Maher Samawi <msamawi2@illinois.edu>
2017-11-17 14:25:44 -0600

    need graphs

--- mp3/part1/training.py
+++ mp3/part1/training.py
@@ -39,6 +39,8 @@ training_images_array = get_file("trainingimages")
 num_images = len(training_images_array)/28
 image_index = 0
 totals = [[[0 for k in range(28)] for j in range(28)] for i in range(10)]
+confusion_matrix = [[0 for i in range(10)] for j in range(10)]
+
 
 while image_index < num_images:
     current_label = int(training_labels_array[image_index])
@@ -48,7 +50,7 @@ while image_index < num_images:
         line = current_image[i]
         for j in range(28):
             pixel = line[j]
-            if (pixel != ' '):
+            if pixel != ' ':
                 totals[current_label][i][j] += 1
     image_index += 1
 
@@ -77,7 +79,7 @@ while image_index < num_test_images:
             line = test_image[row]
             for col in range(28):
                 pixel = line[col]
-                if (pixel != ' '):
+                if pixel != ' ':
                     class_prob[class_index] += math.log(totals[class_index][row][col])
                 else:
                     class_prob[class_index] += math.log(1 - totals[class_index][row][col])
@@ -85,15 +87,52 @@ while image_index < num_test_images:
     output = class_prob.index(max(class_prob))
     test_output.append(output)
     total_test_labels[expected_label] += 1
-    if (output == expected_label):
+    if output == expected_label:
         num_correct += 1
         test_label_output[expected_label] += 1
     else:
-        print("Misidentified: |" + str(output) + "|" + str(expected_label) + "|")
+        confusion_matrix[output][expected_label] += 1
+        #print("Misidentified: |" + str(output) + "|" + str(expected_label) + "|")
 
 for i in range(len(test_label_output)):
     test_label_output[i] /= total_test_labels[i]
     print("Label " + str(i) + " Accuracy: " + str(test_label_output[i]))
 
-print("number correct: " + str(num_correct))
-print("percentage correct: " + str(num_correct/num_test_images))
+print("Number correct: " + str(num_correct))
+print("Percentage correct: " + str(num_correct/num_test_images))
+
+for i in range(len(confusion_matrix)):
+    for j in range(len(confusion_matrix[i])):
+        confusion_matrix[i][j] = round(confusion_matrix[i][j] / total_test_labels[i], 3)
+
+print("Confusion Matrix")
+
+for i in confusion_matrix:
+    print(i)
+    # print(max(i))
+    # print(i.index((max(i))))
+
+print("Odds Ratios for 9 and 4")
+c1 = 9
+c2 = 4
+c1_arr = [[0 for i in range(28)] for j in range(28)]
+c2_arr = [[0 for i in range(28)] for j in range(28)]
+
+for row in range(len(totals[c1])):
+    for col in range(len(totals[c1][row])):
+        val = math.log(totals[c1][row][col])
+        if val > 0:
+            c1_arr[row][col] = "+"
+        elif val < 0 and val > -1:
+            c1_arr[row][col] = "-"
+        elif val < -1 and val > -2 :
+            c1_arr[row][col] = "#"
+        elif val < -2 and val > -3:
+            c1_arr[row][col] = "*"
+with open('log_likelihood.txt', 'w') as f:
+    for i in c1_arr:
+        for j in i:
+            f.write(str(j))
+
+
+
