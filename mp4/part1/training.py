88eaae5cdf041aa4c6062f6bafb0fa1bfb5a5677
Maher Samawi <msamawi2@illinois.edu>
2017-12-02 18:33:38 -0600

    Part1 should be close to done

--- mp4/part1/training.py
+++ mp4/part1/training.py
@@ -1,9 +1,9 @@
 import math
 
-def get_image(current_index):
+def get_image(current_index, list_with_image):
     current_image = []
     for i in range(28):
-        image_line = training_images_array[current_index * 28 + i]
+        image_line = list_with_image[current_index * 28 + i]
         current_image.append(image_line)
     return current_image
 
@@ -45,20 +45,20 @@ label_counts = [0 for i in range(10)]
 training_labels_array = get_file("traininglabels")
 training_images_array = get_file("trainingimages")
 num_images = int(len(training_images_array)/28)
-print(num_images)
 image_index = 0
 weights = [[[0 for k in range(28)] for j in range(28)] for i in range(10)]
 confusion_matrix = [[0 for i in range(10)] for j in range(10)]
-epochs = 3
+epochs = 5
 current_epoch = 0
 alpha = 1
 decay_rate = 1000
 final_results = [0 for i in range(num_images)]
 # Training
 while current_epoch < epochs:
+    image_index = 0
     while image_index < num_images:
         current_label = int(training_labels_array[image_index])
-        current_image = get_image(image_index)
+        current_image = get_image(image_index, training_images_array)
         label_counts[current_label] += 1
         classified_label = classify(current_image)
         final_results[image_index] = classified_label
@@ -74,4 +74,30 @@ for i in range(len(final_results)):
     if int(final_results[i]) == int(training_labels_array[i]):
         total_correct_training += 1
 
-print("Overall correct: " + str(total_correct_training/num_images))
+print("Overall correct on training data: " + str(total_correct_training/num_images))
+
+# Testing
+test_images_array = get_file("testimages")
+test_labels_array = get_file("testlabels")
+test_output = []
+total_test_labels = [0 for i in range(10)]
+test_label_output = [0 for i in range(10)]
+image_index = 0
+num_correct = 0
+num_test_images = int(len(test_labels_array))
+final_results_testing = [0 for i in range(num_test_images)]
+image_index = 0
+while image_index < num_test_images:
+    current_label = int(test_labels_array[image_index])
+    current_image = get_image(image_index, test_images_array)
+    label_counts[current_label] += 1
+    classified_label = classify(current_image)
+    final_results_testing[image_index] = classified_label
+    image_index += 1
+
+total_correct_test = 0
+for i in range(len(test_labels_array)):
+    if int(final_results_testing[i]) == int(test_labels_array[i]):
+        total_correct_test += 1
+
+print("Overall correct on test data: " + str(total_correct_test/num_test_images))
