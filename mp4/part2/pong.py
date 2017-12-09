ec12eff3673a6f1f0d2e5d827e773bedb0527a32
Taran Saggu <tarandeepsaggu@yahoo.com>
2017-12-08 18:29:51 -0600

    Done with basic game representation

--- mp4/part2/pong.py
+++ mp4/part2/pong.py
@@ -6,15 +6,21 @@ ball_y = 0.5
 velocity_x = 0.03
 velocity_y = 0.01
 paddle_height = 0.2
-paddle_y = 0.5 - paddle_height/2
+paddle_y = (0.5 - paddle_height/2)
 paddle_x = 1
 num_bounces = 0
+game_running = True
 
 board_state = [ball_x, ball_y, velocity_x, velocity_y, paddle_y]
-game_board = [["F" for i in range(12)] for j in range(12)]
+game_board = [[" " for i in range(12)] for j in range(12)]
 
 def update_game():
 	# has the agent tried to move the paddle out of bounds?
+	global paddle_y
+	global ball_x
+	global ball_y
+	global velocity_x
+	global velocity_y
 	if (paddle_y < 0):
 		paddle_y = 0
 	if ((paddle_y + paddle_height) > 1 ):
@@ -46,6 +52,10 @@ def update_game():
 
 		velocity_x = -velocity_x + rand_U
 		velocity_y = velocity_y + rand_V
+	elif(ball_x >= 1):
+		global game_running
+		game_running = False
+
 
 	# check velocity requirements
 	if (math.fabs(velocity_x) < 0.03):
@@ -66,7 +76,7 @@ def update_game():
 		else:
 			velocity_x = 1
 
-def udpate_game_state():
+def update_game_state():
 	board_state[0] = int(ball_x * 12)
 	board_state[1] = int(ball_y * 12)
 	if (velocity_x < 0):
@@ -87,7 +97,25 @@ def udpate_game_state():
 
 	board_x = board_state[0]
 	board_y = board_state[1]
-	game_board = [["F" for i in range(12)] for j in range(12)]
-	game_board[board_y][board_x] = "T"
 
-while(ball_x < 1):
+	global game_board
+	game_board = [[" " for i in range(12)] for j in range(12)]
+	print("Board x: " + str(board_x))
+	print("Board y: " + str(board_y))
+	game_board[board_y][board_x] = "O"
+
+
+def print_board():
+	print("========================== Board ===========================")
+	for row in game_board:
+		print(row)
+	print("============================================================")
+	print("\n")
+
+while(game_running):
+	update_game()
+	if (game_running):
+		update_game_state()
+		print_board()
+	else:
+		print("Game Over!")
