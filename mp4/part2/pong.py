5501ed89edb8f1122a2c93089f6dd3e5dbc8541b
Maher Samawi <msamawi2@illinois.edu>
2017-12-10 19:34:04 -0600

    Algo maybe right?

--- mp4/part2/pong.py
+++ mp4/part2/pong.py
@@ -10,9 +10,22 @@ paddle_y = (0.5 - paddle_height/2)
 paddle_x = 1
 num_bounces = 0
 game_running = True
+num_games = 0
 
 board_state = [ball_x, ball_y, velocity_x, velocity_y, paddle_y]
 game_board = [[" " for i in range(12)] for j in range(12)]
+
+# Variables for Q learning
+learning_rate = 0.1
+discount_factor = 0.5
+r_prime = 0
+# Prev State, Action, and Reward
+s = None
+a = None
+r = 0
+
+Q = {}
+N = {}
 
 
 def update_game():
@@ -54,6 +67,9 @@ def update_game():
 
         velocity_x = -velocity_x + rand_U
         velocity_y = velocity_y + rand_V
+        global r_prime
+        r_prime = 1
+
     elif ball_x >= 1:
         global game_running
         game_running = False
@@ -115,10 +131,105 @@ def print_board():
     print("\n")
 
 
+def init_tables():
+    for i in range(12):
+        for j in range(12):
+            for x in range(2):
+                # +1, -1
+                # 0,   1
+                for y in range(3):
+                    # +1, 0, -1
+                    # 0,  1,  2
+                    for paddle_pos in range(12):
+                        for action in range(3):
+                            # Actions
+                            # -1, 0, 1
+                            #  0, 1, 2
+                            hashed_tuple = hash((i, j, x, y, paddle_pos, action))
+                            Q[hashed_tuple] = 0
+                            N[hashed_tuple] = 0
+    terminal_state = hash((-1, -1, -1, -1, -1, -1, -1))
+    Q[terminal_state] = -1
+
+
+def q_learning_algo(s_prime):
+    global paddle_x
+    global ball_x
+    global s
+    global a
+    global r
+    global r_prime
+    prev_state_hash = hash(tuple(list(s).append(a)))
+    # Terminal State Check
+    if ball_x > paddle_x:
+        Q[prev_state_hash] = r_prime
+    if s is not None:
+        N[prev_state_hash] += 1
+        possible_actions = []
+        for i in range(3):
+            action = hash(tuple(list(s_prime).append(i)))
+            possible_actions.append(Q[action])
+        Q[prev_state_hash] = Q[prev_state_hash] + (learning_rate * N[prev_state_hash]) * (r + discount_factor * max(possible_actions) - Q[prev_state_hash])
+    s = s_prime
+    final_actions = []
+    for i in range(3):
+        action = hash(tuple(list(s_prime).append(i)))
+        final_actions.append(Q[action])
+    a = final_actions.index(max(final_actions))
+    update_learning_decay()
+    r = r_prime
+    r_prime = 0
+    return a
+
+
+def update_learning_decay():
+    # C / (C + N(s, a)
+    global s
+    global a
+    global learning_rate
+    current_state_action = hash(tuple(list(s).append(a)))
+    learning_rate = learning_rate / (N[current_state_action] * learning_rate)
+
+
+def reset_states():
+    global ball_x
+    global ball_y
+    global velocity_x
+    global velocity_y
+    global paddle_x
+    global paddle_height
+    global paddle_y
+    global num_bounces
+    global game_running
+    global num_games
+    ball_x = 0.5
+    ball_y = 0.5
+    velocity_x = 0.03
+    velocity_y = 0.01
+    paddle_height = 0.2
+    paddle_y = (0.5 - paddle_height / 2)
+    paddle_x = 1
+    num_bounces = 0
+    game_running = True
+    num_games += 1
+
+
+init_tables()
 while game_running:
+    current_state = (ball_x, ball_y, velocity_x, velocity_y, paddle_y)
+    # move_paddle = q_learning_algo(current_state)
+    # if move_paddle == 0:
+    #     print("Going Down")
+    #     paddle_y -= 0.04
+    # elif move_paddle == 1:
+    #     print("Staying")
+    # else:
+    #     print("Going Up")
+    #     paddle_y += 0.04
     update_game()
     if game_running:
         update_game_state()
         print_board()
     else:
         print("Game Over!")
+        #reset_states()
