d2f041659116beda8368ef510e9364047a9ce24c
Taran Saggu <tarandeepsaggu@yahoo.com>
2017-12-11 13:15:46 -0600

    Gui working great, can test games near the end but algorithm still not working

--- mp4/part2/pong.py
+++ mp4/part2/pong.py
@@ -19,9 +19,10 @@ board_state = [ball_x, ball_y, velocity_x, velocity_y, paddle_y]
 game_board = [[" " for i in range(12)] for j in range(12)]
 
 # Variables for Q learning
-learning_rate = 1
-discount_factor = 0.8
+learning_rate = 0.8
+discount_factor = 0.3
 r_prime = 0
+
 # Prev State, Action, and Reward
 s = None
 a = None
@@ -30,7 +31,12 @@ r = 0
 Q = {}
 N = {}
 
+# variables for exploration function
+r_plus = 1 # "optimistic estimate of the best possible reward obtainable in any state"
+Ne = 10 # make the agent try each action-pair state at least Ne times
 
+# debugging flag
+debugging = False
 def update_game():
     # has the agent tried to move the paddle out of bounds?
     global paddle_y
@@ -40,10 +46,15 @@ def update_game():
     global velocity_y
     global num_bounces
     global r_prime
+    global debugging
 
     if paddle_y < 0:
+        if debugging == True:
+            print("Paddle went too high")
         paddle_y = 0
     if (paddle_y + paddle_height) > 1:
+        if debugging == True:
+            print("Paddle went too low")
         paddle_y = 1 - paddle_height
 
     # move the ball
@@ -52,19 +63,27 @@ def update_game():
 
     # has the ball hit the left wall?
     if ball_x < 0:
+        if debugging == True:
+            print("Ball bounced off the left wall")
         ball_x = -ball_x
         velocity_x = -velocity_x
 
     # has the ball hit the top/bottom walls?
     if ball_y < 0:
+        if debugging == True:
+            print("Ball bounced off the top wall")
         ball_y = -ball_y
         velocity_y = -velocity_y
     if ball_y > 1:
+        if debugging == True:
+            print("Ball bounced off the bottom wall")
         ball_y = 2 - ball_y
         velocity_y = -velocity_y
 
     # has the ball hit the paddle?
     if ball_x >= 1 and (ball_y > paddle_y and ball_y < (paddle_y + paddle_height)):
+        if debugging == True:
+            print("Ball hit the paddle!")
         num_bounces += 1
         ball_x = 2 * paddle_x - ball_x
         rand_U = random.uniform(-0.015, 0.015)
@@ -99,6 +118,7 @@ def update_game():
 
 
 def update_game_state():
+    global board_state
     board_state[0] = int(ball_x * 12)
     board_state[1] = int(ball_y * 12)
     board_x = board_state[0]
@@ -130,12 +150,26 @@ def update_game_state():
 
 def print_board():
     global game_board
+    global board_state 
+
     game_board = [[" " for i in range(12)] for j in range(12)]
-    
-    game_board[board+state[0]][board_state[1]] = "O"
+    board_col = board_state[0]
+    board_row = board_state[1]
+    paddle_row = board_state[4]
+    print("X: " + str(board_col) + " Y:" + str(board_row))
+    if (board_col > 11):
+        game_board[board_row][11] = "X"
+    else:
+        game_board[board_row][board_col] = "O"
     print("========================== Board ===========================")
+    i = 0
     for row in game_board:
-        print(row)
+        if i == paddle_row:
+            print(row, end="")
+            print(" | ")
+        else:
+            print(row)
+        i += 1
     print("============================================================")
     print("\n")
 
@@ -160,6 +194,13 @@ def init_tables():
     terminal_state = hash((-1, -1, -1, -1, -1, -1, -1))
     Q[terminal_state] = -1
 
+def exploration_function(u, n):
+    global Ne
+    global r_plus
+    if (n < Ne):
+        return r_plus
+    else:
+        return u
 
 def q_learning_algo(s_prime):
     global paddle_x
@@ -168,12 +209,13 @@ def q_learning_algo(s_prime):
     global a
     global r
     global r_prime
+    global game_running
+
     # Terminal State Check
     if ball_x > paddle_x:
         s_action = s + (a,)
         prev_state_hash = hash(s_action)
         Q[prev_state_hash] = r_prime
-        global game_running
         game_running = False
 
         a = random.choice([0, 1, 2])
@@ -195,20 +237,35 @@ def q_learning_algo(s_prime):
                 print(s_prime_action)
             possible_actions.append(Q[action])
         term2 = (learning_rate * N[prev_state_hash])
-        term3 = (r + discount_factor * max(possible_actions) - Q[prev_state_hash])
+        term3 = (r + (discount_factor * max(possible_actions)))
         val = Q[prev_state_hash] + term2 * term3
-        # print(val)
         Q[prev_state_hash] = val
         update_learning_decay()
 
     s = s_prime
     final_actions = []
+    final_NSAs = []
     for i in range(3):
         s_prime_action = s_prime + (i,)
-        action = hash(s_prime_action)
-        final_actions.append(Q[action])
-    a = final_actions.index(max(final_actions))
+        hashed = hash(s_prime_action)
+        final_actions.append(Q[hashed])
+        final_NSAs.append(N[hashed])
+
+    # exploration function (where we decide what action we want to perform)
+    
+    # greedy approach
+    # a = final_actions.index(max(final_actions))
+
+    # random approach
     # a = random.choice([0, 1, 2])
+
+    # actual exploration function
+    exploration_actions = []
+    for i in range(3):
+        exploration_actions.append(exploration_function(final_actions[i], final_NSAs[i]))
+
+    a = exploration_actions.index(max(exploration_actions))
+
     r = r_prime
     r_prime = 0
     return a
@@ -219,10 +276,13 @@ def update_learning_decay():
     global s
     global a
     global learning_rate
-    c = 1000
+    c = 1
     s_action = s + (a,)
     current_state_action = hash(s_action)
-    learning_rate = c / (N[current_state_action] * c)
+    num_times = N[current_state_action]
+    # print("Action: " + str(s_action) + " with NSA val: " + str(num_times))
+    learning_rate = c / (num_times * c)
+    # print("learning rate: " + str(learning_rate))
 
 
 def reset_states():
@@ -279,3 +339,33 @@ while game_running and num_games < 10000:
 
 print("Average number of bounces per game: " + str(total_bounces/num_games))
 print("Max bounces: " + str(max_bounces))
+
+# test run
+debugging = True
+reset_states()
+num_games = 0
+while game_running and num_games < 1:
+    update_game_state()
+    print_board()
+    current_state = tuple(board_state)
+    move_paddle = q_learning_algo(current_state)
+    #print("Move paddle is: " + str(move_paddle))
+    if move_paddle == 0:
+        # print("Going Down")
+        paddle_y -= 0.04
+    elif move_paddle == 1:
+        pass
+        # print("Staying")
+    else:
+        # print("Going Up")
+        paddle_y += 0.04
+    update_game()
+    if game_running:
+        pass
+        #print_board()
+    else:
+        if num_bounces > max_bounces:
+            max_bounces = num_bounces
+        # print("Game Over! Num bounces:  " + str(num_bounces))
+        total_bounces += num_bounces
+        reset_states()
